359fb7160dbf5bc55bed1813592ec36d40b20586
ilya <ilyaeruh@gmail.com>
2022-02-11 19:30:51 +0200

    github pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   devtool: 'eval-source-map',
-  mode: 'development',
+  mode: 'production',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
 };
